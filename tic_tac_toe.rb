90b04641087bf9e36f3a3fbc9ebffdfd054b1417
Patrick <patrickfeeneytamayo@gmail.com>
2023-09-29 12:25:04 +0100

    Fix bug. Didn't detect all wins

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -3,10 +3,12 @@ class Board
     @board_hash = create_hash
   end
 
-  def reset
-
+  def valid_choice?(choice)
+    ((1..9).include? board_hash[choice]) || (return false)
+    ((1..9).include? choice) || (return false)
+    true
   end
-  
+
   def game_over?
     if check_win || draw?
       true
@@ -19,8 +21,7 @@ class Board
     check_win
   end
 
-  def draw
-    puts "\n\n"
+  def print
     2.times do |i|
       puts "#{board_hash[1 + 3 * i]} | #{board_hash[2 + 3 * i]} | #{board_hash[3 + 3 * i]}"
       puts '- + - + -'
@@ -30,42 +31,27 @@ class Board
 
   def update(number, symbol)
     @board_hash[number] = symbol
-    draw
-  end
-
-  def test_update_hash
-    board_hash[7] = 'X'
-    board_hash[8] = 'X'
-    board_hash[9] = 'X'
-  end
-
-  def test_update_hash2
-    board_hash[1] = 'X'
-    board_hash[2] = 'O'
-    board_hash[3] = 'X'
-    board_hash[4] = 'X'
-    board_hash[5] = 'O'
-    board_hash[6] = 'X'
-    board_hash[7] = 'O'
-    board_hash[8] = 'X'
-    board_hash[9] = 'O'
   end
 
   private
+
+  attr_reader :board_hash
 
   def check_win
     3.times do |i|
       if board_hash[1 + 3 * i] == board_hash[2 + 3 * i] && board_hash[2 + 3 * i] == board_hash[3 + 3 * i]
         return board_hash[1 + 3 * i]
       elsif board_hash[1 + 1 * i] == board_hash[4 + 1 * i] && board_hash[4 + 1 * i] == board_hash[7 + 1 * i]
-        return board_hash[1 * i]
+        return board_hash[1 + 1 * i]
       end
     end
     if board_hash[1] == board_hash[5] && board_hash[5] == board_hash[9]
       return board_hash[1]
-    elsif board_hash[4] == board_hash[5] && board_hash[5] == board_hash[3]
-      return board_hash[4]
+    elsif board_hash[7] == board_hash[5] && board_hash[5] == board_hash[3]
+      return board_hash[7]
     end
+
+    false
   end
 
   def draw?
@@ -73,10 +59,6 @@ class Board
       ((1..9).include? v) && (return false)
     end
     true
-  end
-
-  def board_hash
-    @board_hash
   end
 
   def create_hash
@@ -89,36 +71,24 @@ class Board
 end
 
 class Player
-  #attr_accessor :choice
+  attr_reader :choice, :character
 
   def initialize(character)
     @character = character
   end
 
-
-  def get_input
+  def set_choice
     @choice = gets.chomp
   end
 
-  def input
-    @choice
-  end
-
-  def character
-    @character
-  end
-
-  private
-  def choice=(number)
-    @choice = number
-  end
-
-  def choice
-    @choice
+  def print_player
+    puts "\nPlayer's #{character} turn >> "
   end
 end
 
 class Game
+  attr_reader :x_player, :o_player, :board
+
   def initialize
     @board = Board.new
     @x_player = Player.new('X')
@@ -128,50 +98,40 @@ class Game
 
   def start
     puts 'Starting game...'
-    board.draw
+    current_player.print_player
+    board.print
     while board.game_over? == false
-      current_player.get_input
-      #puts "Player:#{current_player.character} chose #{current_player.input}"
-      p current_player.input
-      p current_player.character
-      board.update(current_player.input.to_i, current_player.character)
-      self.current_player =
-        if current_player == x_player
-          #puts 'Setting player to O'
-          o_player
-        else
-          #puts 'Setting player to X'
-          x_player
-        end
+      current_player.set_choice
+      if board.valid_choice?(current_player.choice.to_i)
+        board.update(current_player.choice.to_i, current_player.character)
+        self.current_player =
+          if current_player == x_player
+            o_player
+          else
+            x_player
+          end
+      else
+        puts 'Not valid. Try again...'
+      end
+      current_player.print_player
+      board.print
     end
-  end
 
-  def x_player
-    @x_player
-  end
-
-  def o_player
-    @o_player
-  end
-
-  def board
-    @board
+    print_result
   end
 
   private
-  def current_player
-    @current_player
-  end
 
-  def current_player=(player)
-    @current_player = player
+  attr_accessor :current_player
+
+  def print_result
+    if board.winner
+      puts "Player #{board.winner} won!"
+    else
+      puts 'Draw!!!'
+    end
   end
 end
 
 game = Game.new
 game.start
-# game.x_player.get_input
-# p game.x_player.input
-#game.board.draw
-#game.board.test_update_hash2
-
